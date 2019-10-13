ef3573a9a7c2b93d7dcb8738bda4d86a23b5b2e3
BojanKocijan <kocijan@gmail.com>
2019-10-13 10:31:03 +0200

    Comit clean up

---
+++ fullScaleApps/stop-watch/src/Timmer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Timmer extends Component {
+  render (){
+    return (
+      <div>
+        <h2>Stop watch app</h2>
+
+      </div>
+
+
+    );
+  }
+}
+
+export default Timmer;
