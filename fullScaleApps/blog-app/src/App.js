1bde7abe52ab4f167c5574c0c1f6a2305ef3292a
BojanKocijan <kocijan@gmail.com>
2019-10-18 10:05:46 +0200

    Update App.js

--- fullScaleApps/blog-app/src/App.js
+++ fullScaleApps/blog-app/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 // Main application
 import './App.css';
 // Components
-import Navbar from './Components/Navbar';
+import Bojan from './Components/Navbar';
 import HeaderCarousel from './Components/HeaderCarousel';
 import Container from './Components/Container';
 import Footer from './Components/Footer';
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <Bojan />
         <HeaderCarousel />
         <Container />
         <Footer />
