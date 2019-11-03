79818fd90fc15fc2fe7108d509f2628c56ede266
BojanKocijan <kocijan@gmail.com>
2019-11-03 11:06:03 +0100

    Update css clear

--- getsby-app/src/pages/index.js
+++ getsby-app/src/pages/index.js
@@ -3,8 +3,8 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>How to <br />design and code React apps</h1>
+    <p>Complete course about the best tools and design</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
